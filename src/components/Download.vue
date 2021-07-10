a5a68eee285072f03c5123287effc4a85da29f82
Kelvin Chua <chuakid@gmail.com>
2021-07-10 14:08:59 +0800

    formatting

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -2,8 +2,7 @@
   <div class="transition-all bg-gray-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <h2 class="text-3xl font-medium mb-3">Download</h2>
     <div>
-      <label class="">File: </label
-      ><span class="font-medium">{{ filename }}</span>
+      <label class="">File: </label><span class="font-medium">{{ filename }}</span>
     </div>
     <div>
       Expires in
@@ -12,27 +11,11 @@
     </div>
     <div v-if="passwordNeeded">
       <label class="block font-medium" name="password">Password</label>
-      <input
-        v-model="password"
-        id="password"
-        title="password"
-        class="w-full border px-1 shadow-md"
-        type="password"
-      />
+      <input v-model="password" id="password" title="password" class="w-full border px-1 shadow-md" type="password" />
     </div>
     <button
       @click="download"
-      class="
-        mt-4
-        transition-all
-        w-full
-        text-white
-        rounded
-        bg-blue-500
-        hover:bg-blue-700
-        px-2
-        py-2
-      "
+      class="mt-4 transition-all w-full text-white rounded bg-blue-500 hover:bg-blue-700 px-2 py-2"
     >
       Download
     </button>
@@ -46,17 +29,13 @@ export default {
     download() {
       if (this.passwordNeeded) {
         axios
-          .post(
-            import.meta.env.VITE_HOST +
-              "/api/gettoken/" +
-              this.$route.params.file_id,
-            {
-              password: String(this.password),
-            }
-          )
+          .post(import.meta.env.VITE_HOST + "/api/gettoken/" + this.$route.params.file_id, {
+            password: String(this.password),
+          })
           .then((response) => {
             let link = document.createElement("a");
-            link.href = import.meta.env.VITE_HOST +
+            link.href =
+              import.meta.env.VITE_HOST +
               "/api/downloadfilewithtoken/" +
               this.$route.params.file_id +
               "/" +
@@ -84,16 +63,12 @@ export default {
   },
   mounted() {
     axios
-      .get(
-        import.meta.env.VITE_HOST +
-          "/api/file_info/" +
-          this.$route.params.file_id
-      )
+      .get(import.meta.env.VITE_HOST + "/api/file_info/" + this.$route.params.file_id)
       .then((response) => {
-          this.filename = response.data['filename']
-          this.hours = response.data['hours']
-          this.minutes = response.data['minutes']
-          this.passwordNeeded = response.data['passwordneeded']
+        this.filename = response.data["filename"];
+        this.hours = response.data["hours"];
+        this.minutes = response.data["minutes"];
+        this.passwordNeeded = response.data["passwordneeded"];
       })
       .catch((e) => {});
   },
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -4,34 +4,15 @@
       <h2 class="text-3xl font-medium text-center mb-3">Upload</h2>
       <div class="mb-2">
         <label class="block font-medium">File</label
-        ><input
-          name="file"
-          v-on:change="fileChanged"
-          class="cursor-pointer"
-          type="file"
-        />
+        ><input name="file" v-on:change="fileChanged" class="cursor-pointer" type="file" />
       </div>
       <div class="mb-2">
         <label class="block font-medium">Password (optional)</label>
-        <input
-          class="px-1 w-full shadow-md border"
-          type="password"
-          v-model="password"
-        />
+        <input class="px-1 w-full shadow-md border" type="password" v-model="password" />
       </div>
       <button
         @click="submit"
-        class="
-          w-full
-          mt-2
-          py-2
-          px-2
-          bg-blue-500
-          rounded
-          hover:bg-blue-700
-          text-white
-          transition
-        "
+        class="w-full mt-2 py-2 px-2 bg-blue-500 rounded hover:bg-blue-700 text-white transition"
         id="submit"
         type="button"
       >
@@ -41,51 +22,28 @@
         <div class="flex mb-2 items-center justify-between">
           <div>
             <span
-              class="
-                text-xs
-                font-semibold
-                inline-block
-                py-1
-                px-2
-                uppercase
-                rounded-full
-                text-indigo-600
-                bg-indigo-200
-              "
+              class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-indigo-600 bg-indigo-200"
             >
               Upload Progress
             </span>
           </div>
           <div class="text-right">
-            <span class="text-xs font-semibold inline-block text-indigo-600">
-              {{ uploadProgress }}%
-            </span>
+            <span class="text-xs font-semibold inline-block text-indigo-600"> {{ uploadProgress }}% </span>
           </div>
         </div>
-        <div
-          class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200"
-        >
+        <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
           <div
             :style="progressBarWidth"
-            class="
-              shadow-none
-              flex flex-col
-              text-center
-              whitespace-nowrap
-              text-white
-              justify-center
-              bg-indigo-500
-            "
+            class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"
           ></div>
         </div>
       </div>
       <div v-if="uploadSuccess" class="rounded bg-indigo-900 hover:bg-indigo-500 pr-2 mt-2 transition">
-        <span class="rounded inline-block bg-indigo-700 shadow-none text-white px-3 mr-2 ">Link</span>
+        <span class="rounded inline-block bg-indigo-700 shadow-none text-white px-3 mr-2">Link</span>
         <a class="text-white" :href="downloadLink">
           {{ downloadLink }}
         </a>
       </div>
-      
     </form>
   </div>
 </template>
@@ -93,6 +51,7 @@
 <script>
 import axios from "axios";
 export default {
+  mounted() {},
   computed: {
     progressBarWidth() {
       return "width: " + this.uploadProgress + "%";
@@ -113,9 +72,7 @@ export default {
           },
           onUploadProgress: (progressEvent) => {
             this.uploadStarted = true;
-            this.uploadProgress = Math.round(
-              (progressEvent.loaded / progressEvent.total) * 100
-            );
+            this.uploadProgress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
             // console.log(progressEvent.loaded / progressEvent.total),
           },
         })
